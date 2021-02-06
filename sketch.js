739d917cf4ea902905f611ffdf938a528f022fc8
vedant-pro <72551024+vedant-pro@users.noreply.github.com>
2021-02-06 14:59:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,91 @@
+var backImage,backgr;
+var player, player_running;
+var ground,ground_img;
+
+var END =0;
+var PLAY =1;
+var gameState = PLAY;
+
+function preload(){
+  backImage=loadImage("jungle.jpg");
+  player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+  bananaImg=loadImage("banana.png")
+  obstacleImage=loadImage("stone.png")
+}
+
+function setup() {
+  createCanvas(800,400);
+  
+  backgr=createSprite(0,0,800,400);
+  backgr.addImage(backImage);
+  backgr.scale=1.5;
+  backgr.x=backgr.width/2;
+  backgr.velocityX=-4;
+  
+  player = createSprite(100,340,20,50);
+  player.addAnimation("Running",player_running);
+  player.scale = 0.1;
+  
+  ground = createSprite(400,350,800,10);
+  ground.x=ground.width/2;
+  ground.visible=false;
+  bananaGroup=new Group()
+  obstacleGroup=new Group()
+}
+
+function draw() { 
+  background(0);
+
+  if(gameState===PLAY){
+  
+  if(backgr.x<100){
+    backgr.x=backgr.width/2;
+  }
+  
+    if(keyDown("space") ) {
+      player.velocityY = -12;
+    }
+    player.velocityY = player.velocityY + 0.8;
+  
+    player.collide(ground);
+    spawnFood()
+  }
+  if(bananaGroup.isTouching(player)){
+    bananaGroup.destroyEach()
+    player.scale+= +0.1
+  }
+  if(obstacleGroup.isTouching(player)){
+    gameState=END
+  }
+  else if(gameState===END){
+    backgr.velocityX=0
+    player.visible=false
+    bananaGroup.destroyEach()
+    obstacleGroup.destroyEach()
+    textSize(30)
+    fill(255)
+    text("game over",300,220)
+  }
+  drawSprites();
+}
+function spawnFood(){
+  if(World.frameCount%80===0){
+    banana=createSprite(600,250,40,10)
+    banana.y=random(120,200)
+    banana.addImage(bananaImg)
+    banana.scale=0.05
+    banana.velocityX=-4
+    banana.setLifetime=300
+    bananaGroup.add(banana)
+  }
+}
+function rock(){
+  if(World.frameCount%300===0){
+    obstacle=createSprite(600,480,20,20)
+    obstacle.addImage(obstacleImage)
+    obstacle.scale=0.2
+    obstacle.velocityX=-3
+    obstacle.setLifetime=200
+    obstacleGroup.add(obstacle)
+  }
+}
